6a51c2202451010345717f08b86a8ba15e1ba4b4
Kamil <30302558+kamilz92@users.noreply.github.com>
2024-04-10 13:20:52 +0200

    Add files via upload

--- CLI/script.sh
+++ CLI/script.sh
@@ -5,7 +5,7 @@ CONFIG_FILE="config.json"
 if [[ ! -f "$CONFIG_FILE" ]]; then
     echo "config.json' file does not exist."
     echo "Creating 'config.json' file."
-    echo '{"HOME_PATH": "", "DOCKER_DESKTOP_PATH": "", "EMPAIA_PATH": "", "IMAGE_PATH": ""}' > "$CONFIG_FILE"
+    echo '{"EXIT_PATH": "", "DOCKER_DESKTOP_PATH": "", "EMPAIA_PATH": ""}' > "$CONFIG_FILE"
 fi
 
 # Check if jq is installed
@@ -23,10 +23,10 @@ fi
 echo "jq is installed."
 # Read the DOCKER_DESKTOP_PATH and EMPAIA_PATH from the JSON configuration file.
 # jq is a command-line JSON processor.
-HOME_PATH=$(jq -r '.HOME_PATH' "$CONFIG_FILE")
+EXIT_PATH=$(jq -r '.EXIT_PATH' "$CONFIG_FILE")
 DOCKER_DESKTOP_PATH=$(jq -r '.DOCKER_DESKTOP_PATH' "$CONFIG_FILE")
 EMPAIA_PATH=$(jq -r '.EMPAIA_PATH' "$CONFIG_FILE")
-IMAGE_PATH=$(jq -r '.IMAGE_PATH' "$CONFIG_FILE")
+
 # Function to start the EMPAIA application
 # - config_file: The path to the JSON file.
 # - key: The key to update in the JSON file.
@@ -38,12 +38,19 @@ update_json(){
     python3 -c "import json; data = json.load(open('$config_file')); data['$key'] = r'$value'; json.dump(data, open('$config_file', 'w'))"
 }
 
+
+#
+if [[ -z $EXIT_PATH ]]; then
+    EXIT_PATH=$(pwd)
+    update_json $CONFIG_FILE "EXIT_PATH" $EXIT_PATH
+fi
+
 # This function starts the EMPAIA application.
 # If EMPAIA_PATH is not set, it asks the user to enter it and updates the JSON file.
 # TO DO: Add a check to see if the EMPAIA service is already running,
 start_app(){
 
-    # Check if the EMPAIA_PATH is set
+    # Check if the EMPAIA_PATH is set,????
     if [[ -z $EMPAIA_PATH ]]; then
         echo "Path to EMPAIA not found."
         read -r -p "Please enter the path to EMPAIA: " EMPAIA_PATH
@@ -51,6 +58,13 @@ start_app(){
     fi
     # Change the directory to the EMPAIA_PATH
     cd $EMPAIA_PATH || exit
+
+
+    if [[ ! -d "images" ]]; then
+        echo "Images directory does not exist in $EMPAIA_PATH, creating it."
+        mkdir images
+        ls
+    fi
     # check if wsi-mount-points.json
     if [[ ! -f "wsi-mount-points.json" ]]; then
         echo "Error: 'wsi-mount-points.json' file does not exist in $EMPAIA_PATH"
@@ -59,7 +73,7 @@ start_app(){
         if [[ $answer == "yes" ]]; then
             echo "Creating 'wsi-mount-points.json' file"
             # Construct the key
-            key="${EMPAIA_PATH}/eats/images"
+            key="${EMPAIA_PATH}/images"
             # Create the JSON object
             json=$(jq -n \
               --arg path "$key" \
@@ -77,20 +91,12 @@ start_app(){
 }
 # Function to add picture to the EMPAIA
 add_picture(){
-    # Check if the image path is empty
-    if [[ -z "$IMAGE_PATH" ]]; then
-        read -r -p "Please enter the image directory you want to use: " IMAGE_PATH
-        # Update the IMAGE_PATH in the config.json file
-        update_json "$CONFIG_FILE" "IMAGE_PATH" "$IMAGE_PATH"
-    else
-        echo "Image path is $IMAGE_PATH. You can change it manually if needed."
-    fi
-
-    cd "$IMAGE_PATH" || { echo "Error: Failed to change directory to $IMAGE_PATH"; return 1; }
+    cd "$EMPAIA_PATH" || { echo "Error: Failed to change directory to $IMAGE_PATH"; return 1; }
 
     # Check if the images directory exists
     if [[ ! -d "images" ]]; then
-        echo "Error: 'images' directory does not exist in $IMAGE_PATH"
+        echo "Error: 'images' directory does not exist in $EMPAIA_PATH, creating it. Start again and add some pictures there."
+        mkdir -p images
         return 1
     fi
 
@@ -102,7 +108,6 @@ add_picture(){
         echo "Error: Image $IMAGE_NAME does not exist"
         return 1
     fi
-    
     
     # Remove the type from the image name
     IMAGE_NAME_NO_TYPE="${IMAGE_NAME%.*}"
@@ -134,7 +139,7 @@ add_picture(){
 }
 
 # create EADS FUNCTION
-create_eads(){
+create_eads_manually(){
         echo '{
         "$schema": "https://gitlab.com/empaia/integration/definitions/-/raw/main/ead/ead-schema.v3.json",
         "name": "My Cool Medical AI Algorithm",
@@ -163,30 +168,69 @@ create_eads(){
         }
     }' > ead.json
 }
-# TODO
-# ASk about app path
-# create app path if not exist
-# ask user about if he wants creates eads.json with script or manually
-# 
-#
-add_app(){
-    read -r -p "Please enter the path to the app you want to use: " APP_PATH
-    # Update the APP_PATH in the config.json file
-    update_json "$CONFIG_FILE" "APP_PATH" "$APP_PATH"
-    # Change the directory to the APP_PATH
-    cd "$APP_PATH" || { echo "Error: Failed to change directory to $APP_PATH"; return 1; }
+#TODO
+create_eads_from_file(){
+        echo '{
+        "$schema": "https://gitlab.com/empaia/integration/definitions/-/raw/main/ead/ead-schema.v3.json",
+        "name": "My Cool Medical AI Algorithm",
+        "name_short": "Cool App",
+        "namespace": "org.empaia.helse_vest_piv.cool_app.v3.1",
+        "description": "Does super advanced AI stuff, you know...",
+        "io": {
+            "my_wsi": {
+                "type": "wsi"
+            },
+            "my_quantification_result": {
+                "type": "float",
+                "description": "Human readable text, e.g. super important metric",
+                "reference": "io.my_wsi"
+            }
+        },
+        "modes": {
+            "standalone": {
+                "inputs": [
+                    "my_wsi"
+                ],
+                "outputs": [
+                    "my_quantification_result"
+                ]
+            }
+        }
+    }' > ead.json
+}
 
-    #ask user if he wants to create eads.json
-    read -r -p "Would you like to create eads.json file? (yes/no): " answer
-    if [[ $answer == "yes" ]]; then
-        create_eads
-        cat ead.json
+ 
+# add app to eats
+add_app(){
+    cd "$EMPAIA_PATH" || { echo "Error: Failed to change directory to $EMPAIA_PATH"; return 1; }
+    # Ask user if he has all the files needed
+    read -r -p "Do you have all the files needed? (eads.json etc.) (yes/no): " answer
+    if [[ $answer == "no" ]]; then
+        # ask user if he wants to create eads.json manually or form json file
+        # TODO
     fi
+    # docker logic TODO
+
+
+    read -r -p "Please enter APP name: " navn
+    eats apps register ead.json "$navn" > app.env
+    export $(xargs < app.env)
+    eats jobs register $APP_ID ./inputs > job.env
+    export $(xargs < job.env)
+    eats jobs run ./job.env
+    eats jobs wait $EMPAIA_JOB_ID
+    eats jobs status $EMPAIA_JOB_ID
+}
+# exits CLI in eats
+exit_cli_eats(){
+    printf "\nExiting CLI in eats.\n"
+    cd $EMPAIA_PATH
 }
 #exit cli
 exit_cli(){
     printf "\nExiting...\n"
-    cd ~
+    cd $EXIT_PATH
+    return 1
 }
 
 # This function shuts down the EMPAIA service and exits the script.
@@ -219,7 +263,7 @@ fi
 
 
 # Define the menu options
-options=("Initiate EMPAIA service; readies system for deployments." "Add picture." "Add app." "Show help; link to GitHub documentation." "Close CLI, keep service active." "Terminate CLI and EMPAIA service.")
+options=("Initiate EMPAIA service; readies system for deployments." "Add picture." "Add app." "Show help; link to GitHub documentation." "Exit CLI in eats." "Close CLI, keep service active." "Terminate CLI and EMPAIA service.")
 
 # while loop
 while true; do
@@ -242,6 +286,10 @@ while true; do
             "Show help; link to GitHub documentation.")
                 help
                 break
+                ;;
+            "Exit CLI in eats.")
+                exit_cli_eats
+                break 2
                 ;;
             "Close CLI, keep service active.")
                 exit_cli
